b2bd74fb4594ca5fb8b77782db5479cf0352ef8f
Michael Jennings <robertmjenningsjr@gmail.com>
2025-09-25 19:54:31 -0700

    alpha welcome and updated deprecated lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,11 +16,19 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.pylsp.setup({})
-			lspconfig.clangd.setup({})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			-- local lspconfig = require("lspconfig")
+			-- lspconfig.lua_ls.setup({})
+			-- lspconfig.pylsp.setup({})
+			-- lspconfig.clangd.setup({})
+			local servers = { lua_ls = {}, pylsp = {}, clangd = {} }
+			for server, settings in pairs(servers) do
+				vim.lsp.config(server, settings)
+				vim.lsp.enable(server)
+			end
+			-- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "K", function()
+				vim.lsp.buf.hover({ border = "rounded" })
+			end, { desc = "LSP Hover" })
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
